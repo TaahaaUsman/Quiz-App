33cf7ec7f9b1ff6ceb28fa532c41739d3d3927b1
Taahaa Usman <you@example.com>
2025-06-05 23:49:34 +0500

    Login problem resolve

--- components/Register.jsx
+++ components/Register.jsx
@@ -58,7 +58,7 @@ export default function RegisterPage() {
 
     setLoading(true);
     try {
-      const response = await fetch("/api/auth/register", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/register`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
